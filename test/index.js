df9073b55b1a710e59fa40387c155dc0db723421
Adam K Dean <adamkdean@googlemail.com>
2016-08-06 18:55:37 +0100

    preblog cleanup

--- test/index.js
+++ test/index.js
@@ -3,14 +3,47 @@ const path = require('path')
 const lib = require('../lib/')
 const ImageProcessor = new lib()
 
-const inputPath = path.join(__dirname, '/../images/02 - UeRFpoD.jpg')
-const outputPath = path.join(__dirname, '/../output/02 - UeRFpoD.jpg')
+console.log('[Notice] These can take upwards of 10 seconds. Please be patient.')
 
-describe('pixel processing', function () {
+describe('Process images', function () {
 
   this.timeout(30000)
 
-  it('should process file that exists', (done) => {
+  it.skip('should process a.jpg', (done) => {
+    const inputPath = path.join(__dirname, '/../images/a.jpg')
+    const outputPath = path.join(__dirname, '/../output/a.jpg')
+    ImageProcessor.processImage(inputPath, outputPath, () => {
+      done()
+    })
+  })
+
+  it.skip('should process b.jpg', (done) => {
+    const inputPath = path.join(__dirname, '/../images/b.jpg')
+    const outputPath = path.join(__dirname, '/../output/b.jpg')
+    ImageProcessor.processImage(inputPath, outputPath, () => {
+      done()
+    })
+  })
+
+  it.skip('should process c.jpg', (done) => {
+    const inputPath = path.join(__dirname, '/../images/c.jpg')
+    const outputPath = path.join(__dirname, '/../output/c.jpg')
+    ImageProcessor.processImage(inputPath, outputPath, () => {
+      done()
+    })
+  })
+
+  it.skip('should process winter.jpg', (done) => {
+    const inputPath = path.join(__dirname, '/../images/winter.jpg')
+    const outputPath = path.join(__dirname, '/../output/winter.jpg')
+    ImageProcessor.processImage(inputPath, outputPath, () => {
+      done()
+    })
+  })
+
+  it('should process rain.jpg', (done) => {
+    const inputPath = path.join(__dirname, '/../images/rain.jpg')
+    const outputPath = path.join(__dirname, '/../output/rain.jpg')
     ImageProcessor.processImage(inputPath, outputPath, () => {
       done()
     })
